980d2b16eeeb58353e8184126acc8f0b5f3b38a7
Lan Wang <yexiaosu0313@gmail.com>
2023-02-20 22:59:28 -0500

    Time: 33 ms (37.69%), Space: 22.4 MB (16.37%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while (l < r) {
+            int p = (l+r)/2;
+            bool higherEven = ((r-p) % 2== 0);
+            if (nums[p] == nums[p+1]) {
+                higherEven ? (l=p+2) : (r=p-1);
+            } else if (nums[p] == nums[p-1]) {
+                higherEven ? (r=p-2) : (l=p+1);
+            } else {
+                return nums[p];
+            }
+        }
+        return nums[l];
+    }
+};
