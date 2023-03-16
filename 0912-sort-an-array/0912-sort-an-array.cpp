061532a6acc842041c3a0d93631f9b6edeaa1136
Lan Wang <yexiaosu0313@gmail.com>
2023-03-16 16:41:42 -0400

    Time: 213 ms (54.15%), Space: 68.6 MB (41.75%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,54 +1,54 @@
 class Solution {
-    void merge(vector<int>& nums, vector<int>& left, vector<int>& right) {
-        if (left.back() <= right.front()) {
-            int k = 0;
-            for (int i = 0; i < left.size(); i++) {
-                nums[k] = left[i];
-                k++;
-            }
-            for (int i = 0; i < right.size(); i++) {
-                nums[k] = right[i];
-                k++;
-            }
+    void merge(vector<int>& nums, int left, int right, int mid, vector<int> &tempArr) {
+        if (nums[mid] <= nums[mid+1]) {
+            return;
         }
-        int i = 0, j = 0, k = 0;
-        while (i != left.size() && j != right.size()) {
-            if (right[j] >= left[i]) {
-                nums[k] = left[i];
-                i++;
+        int start1 = left;
+        int start2 = mid + 1;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        for (int i = 0; i < n1; i++) {
+            tempArr[start1 + i] = nums[start1 + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            tempArr[start2 + i] = nums[start2 + i];
+        }
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (tempArr[start1 + i] <= tempArr[start2 + j]) {
+                nums[k] = tempArr[start1 + i];
+                i += 1;
             } else {
-                nums[k] = right[j];
-                j++;
+                nums[k] = tempArr[start2 + j];
+                j += 1;
             }
-            k++;
+            k += 1;
         }
-        if ((left.begin() + i) == left.end()) {
-            for (j = j; j < right.size(); j++) {
-                nums[k] = right[j];
-                k++;
-            }
-        } else {
-            for (i = i; i < left.size(); i++) {
-                nums[k] = left[i];
-                k++;
-            }
+        while (i < n1) {
+            nums[k] = tempArr[start1 + i];
+            i += 1;
+            k += 1;
+        }
+        while (j < n2) {
+            nums[k] = tempArr[start2 + j];
+            j += 1;
+            k += 1;
         }
     }
     
-    void mergeSort(vector<int>& nums) {
-        if (nums.back() >= nums.front() && nums.size() <= 2) {
+    void mergeSort(vector<int>& nums, int left, int right, vector<int> &tempArr) {
+        if (left >= right) {
             return;
         }
-        int mid = nums.size() / 2;
-        vector<int> left(nums.begin(), nums.begin() + mid);
-        vector<int> right(nums.begin() + mid, nums.end());
-        mergeSort(left);
-        mergeSort(right);
-        merge(nums, left, right);
+        int mid = (left + right) / 2;
+        mergeSort(nums, left, mid, tempArr);
+        mergeSort(nums, mid+1, right, tempArr);
+        merge(nums, left, right, mid, tempArr);
     }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums);
+        vector<int> temporaryArray(nums.size());
+        mergeSort(nums, 0, nums.size() - 1, temporaryArray);
         return nums;
     }
 };
