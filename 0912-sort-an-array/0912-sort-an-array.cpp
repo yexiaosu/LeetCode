43862cf16f8c96de33795f9c36be3deec2641a52
Lan Wang <yexiaosu0313@gmail.com>
2023-03-16 16:19:13 -0400

    Time: 640 ms (19.65%), Space: 169.7 MB (14.27%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    void merge(vector<int>& nums, vector<int>& left, vector<int>& right) {
+        int i = 0, j = 0, k = 0;
+        while (i != left.size() && j != right.size()) {
+            if (right[j] >= left[i]) {
+                nums[k] = left[i];
+                i++;
+            } else {
+                nums[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        if ((left.begin() + i) == left.end()) {
+            for (j = j; j < right.size(); j++) {
+                nums[k] = right[j];
+                k++;
+            }
+        } else {
+            for (i = i; i < left.size(); i++) {
+                nums[k] = left[i];
+                k++;
+            }
+        }
+    }
+    
+    void mergeSort(vector<int>& nums) {
+        if (nums.back() >= nums.front() && nums.size() <= 2) {
+            return;
+        }
+        int mid = nums.size() / 2;
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid, nums.end());
+        mergeSort(left);
+        mergeSort(right);
+        merge(nums, left, right);
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums);
+        return nums;
+    }
+};
